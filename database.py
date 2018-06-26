fa346abaf09937bb352eacf0310758037435b0dc
Sterling Anderson <s@sterlinganderson.net>
2018-06-26 12:31:02 -0500

    test

--- database.py
+++ database.py
@@ -64,5 +64,5 @@ class weather_database:
     def select_rain_last_hour(self):
         data=self.db.query(self.last_hour_rainfall_template)
         print("**")
-        print(data[0][1])
+        print(data)
         print("**")
