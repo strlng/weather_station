b0d5a36c3a8e160a60ab1b0d947ee578c23e298d
Sterling Anderson <s@sterlinganderson.net>
2018-06-29 20:40:32 -0500

    getting rid of big while loop

--- weather_station_data.py
+++ weather_station_data.py
@@ -11,8 +11,6 @@ import decimal
 import ds18b20_therm
 import wu_upload
 
-CM_IN_A_KM = 100000.0
-SECS_IN_AN_HOUR = 3600
 ADJUSTMENT = 1.18
 BUCKET_SIZE = 0.2794
 
@@ -35,10 +33,10 @@ def calculate_speed(time_sec):
     circumference_cm = (2 * math.pi) * radius_cm
     rotations = wind_count / 2.0
 
-    dist_km = (circumference_cm * rotations) / CM_IN_A_KM
+    dist_km = (circumference_cm * rotations) / 100000.0
 
     km_per_sec = dist_km / time_sec
-    km_per_hour = km_per_sec * SECS_IN_AN_HOUR
+    km_per_hour = km_per_sec * 3600
 
     return km_per_hour * ADJUSTMENT
 
@@ -60,6 +58,7 @@ rain_sensor.when_pressed = bucket_tipped
 
 wind_speed_sensor = Button(5)
 wind_speed_sensor.when_pressed = spin
+
 def direction(wind_average):
     direction = "Null"
     if wind_average <= 89.9:
@@ -74,39 +73,39 @@ def direction(wind_average):
 
 db = database.weather_database()
 
-while True:
-    start_time = time.time()
-    while time.time() - start_time <= interval:
-        wind_start_time = time.time()
-        reset_wind()
-        while time.time() - wind_start_time <= wind_interval:
-            store_directions.append(wind_direction.get_value())
+#while True:
+start_time = time.time()
+while time.time() - start_time <= interval:
+    wind_start_time = time.time()
+    reset_wind()
+    while time.time() - wind_start_time <= wind_interval:
+        store_directions.append(wind_direction.get_value())
 
-        final_speed = calculate_speed(wind_interval)
-        store_speeds.append(final_speed)
-    wind_average = wind_direction.get_average(store_directions)
-    wind_gust = max(store_speeds)
-    wind_speed = statistics.mean(store_speeds)
-    rainfall = rain_count * BUCKET_SIZE
-    reset_rainfall()
-    humidity = 0
-    pressure = 0
-    ambient_temp = 0
-    ground_temp = 0
-    humidity, pressure, ambient_temp = bme280_sensor.read_all()
-    ds18b20 = ds18b20_therm.DS18B20()
-    ground_temp = ds18b20.read_temp()
-    #print("wind speed: " + str(wind_speed))
-    #print("wind gust: " + str(wind_gust))
-    #print("rainfall: " + str(rainfall))
-    #print("wind direction: " + str(direction(wind_average)))
-    #print("humidity: " + str(humidity)),
-    #print("pressure: " + str(pressure)),
-    #print("ambient_temp: " + str((ambient_temp * (9.0/5.0)) + 32.0 ))
+    final_speed = calculate_speed(wind_interval)
+    store_speeds.append(final_speed)
+wind_average = wind_direction.get_average(store_directions)
+wind_gust = max(store_speeds)
+wind_speed = statistics.mean(store_speeds)
+rainfall = rain_count * BUCKET_SIZE
+reset_rainfall()
+humidity = 0
+pressure = 0
+ambient_temp = 0
+ground_temp = 0
+humidity, pressure, ambient_temp = bme280_sensor.read_all()
+ds18b20 = ds18b20_therm.DS18B20()
+ground_temp = ds18b20.read_temp()
+#print("wind speed: " + str(wind_speed))
+#print("wind gust: " + str(wind_gust))
+#print("rainfall: " + str(rainfall))
+#print("wind direction: " + str(direction(wind_average)))
+#print("humidity: " + str(humidity)),
+#print("pressure: " + str(pressure)),
+#print("ambient_temp: " + str((ambient_temp * (9.0/5.0)) + 32.0 ))
 
-    db.insert(ambient_temp, ground_temp, 0, pressure, humidity, round(wind_average,2), round(wind_speed,2), round(wind_gust,2), round(rainfall,2))
-    rainfall = db.select_rain_last_hour()
-    daily_rainfall = db.select_rain_today()
-    wu_upload.upload_weather_data(humidity, ambient_temp, pressure, ground_temp, wind_speed, wind_gust, wind_average, rainfall, daily_rainfall)
-    store_speeds = []
-    store_directions = []
+db.insert(ambient_temp, ground_temp, 0, pressure, humidity, round(wind_average,2), round(wind_speed,2), round(wind_gust,2), round(rainfall,2))
+rainfall = db.select_rain_last_hour()
+daily_rainfall = db.select_rain_today()
+wu_upload.upload_weather_data(humidity, ambient_temp, pressure, ground_temp, wind_speed, wind_gust, wind_average, rainfall, daily_rainfall)
+store_speeds = []
+store_directions = []
